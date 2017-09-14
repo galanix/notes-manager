709ac506d20a4cb31e9b51737e992c67bdd29509
Oleg <oleg.galanix@gmail.com>
2017-09-14 18:11:58 +0300

    Добавил дефисы к подпапкам. Количество дефисов отображает уровеь вложеностию. Отрендерил дерево папок в левой части приложения.

--- js/structure.js
+++ js/structure.js
@@ -1,21 +1,20 @@
-localStorage.clear();
+// localStorage.clear();
 
 // Creating folders
 
-
 let data = (localStorage.getItem("structure")) 
 ? JSON.parse(localStorage.getItem("structure")) : {
-	"folders": [],
+	"folders": [{
+		"id": "root",
+		"name": "Folders",
+		"children": []
+	}],
 	"notes": {}
 };
 
 let idCounter = (localStorage.getItem("idCounter")) 
 ? localStorage.getItem("idCounter") : 0;
 let select = $("#folders_tree");
-// Amount of dashes = nested level of subfolder
-let dashes = "";
-// Nested lvl in localStorage
-let levelCounter = 0;
 
 
 // Returns nested object 
@@ -38,27 +37,44 @@ function find(source, id) {
     return null;
   }
 
+// Render select list 
+function renderSelect(arr, counter) {
+	for(let i = 0; i < arr.length; i++) {
+		let name = arr[i].name;
+		let id = arr[i].id;
+		
+		if (arr[i].children) {
+			let dashes = "";
+			for (let i = 0; i < counter - 1; i++) {
+				dashes += "-";
+			}
+			select.append(`
+				<option id="${id}" value="${name}">${dashes} ${name}</option>
+				`);
+			dashes = "";
 
-// Render select 
-function renderSelect(obj) {
-	for(key in obj) {
-		let item = obj[key];
-		let name = item.name;
-		let id = item.id;
-		if (item.root) levelCounter = 0;
-
-		select.append(`
-			<option id="${id}" value="${name}">${dashes} ${name}</option>
-			`);
-
-		if (item.children) {
-			levelCounter ++;
-			
-			renderSelect(item.children);
+			renderSelect(arr[i].children, counter + 1);
 		} 
 	}
 }
 
+// Render folder tree
+
+function parseFolders(folders) { // takes a nodes array and turns it into a <ol>
+	let ul = $("<ul>");
+	for(var i = 0; i < folders.length; i++) {
+		ul.append(parseFolder(folders[i]));
+	}
+	return ul;
+}
+
+function parseFolder(folder) { // takes a node object and turns it into a <li>
+	let li = $("<li>");
+	li.html(`<span id="${folder.id}" class="folder_name">
+		<i class="fa fa-caret-right" aria-hidden="true"></i> ${folder.name}</span>`);  
+	if(folder.children) li.append(parseFolders(folder.children));
+	return li;
+}
 
 function updateData() {
 	let folderName =  $("#folder_name").val();
@@ -67,7 +83,6 @@ function updateData() {
 	let newFolder = {
 		"id": idCounter,
 		"name": folderName,
-		"dashes": 0,
 		"children": []
 	};
 
@@ -86,8 +101,10 @@ function updateData() {
 
 }
 
-renderSelect(data.folders);
+
+renderSelect(data.folders, 0);
 idCounter++;
+$(".folders").append(parseFolders(data.folders));
 
 	// if ( $(".folders").children("li").children("ul")[0] === undefined ) {
 	// 	$(".folders").children("li").append(`
@@ -129,8 +146,9 @@ $(".create").on("click", function() {
 	if ( $("#folder_name").val() ) {
 		updateData();
 		select.find("option").not(".select_root").remove();
-		renderSelect(data.folders);
-		console.log(levelCounter);
+		renderSelect(data.folders, 0);
+		$(".folders").find("*").remove();
+		$(".folders").append(parseFolders(data.folders));
 	}
 	
 	$("#popup").fadeOut(500);
@@ -138,88 +156,3 @@ $(".create").on("click", function() {
 
 });
 
-// let folders = 
-// [  
-// {  
-// 	"id":0,
-// 	"name":"Folder_1",
-// 	"children":[  
-// 	{  
-// 		"id":1,
-// 		"name":"Folder_1.1",
-// 		"children":[  
-// 		{  
-// 			"id":2,
-// 			"name":"Folder_1.1.1",
-// 			"children":[  
-// 			{  
-// 				"id":8,
-// 				"name":"Folder_1.1.1.1",
-// 				"children":[  
-
-// 				]
-// 			}
-// 			],
-// 			"isParent":true
-// 		},
-// 		{  
-// 			"id":3,
-// 			"name":"Folder_1.1.2",
-// 			"children":[  
-
-// 			]
-// 		}
-// 		],
-// 		"isParent":true
-// 	}
-// 	],
-// 	"isParent":true
-// },
-// {  
-// 	"id":4,
-// 	"name":"Folder_2",
-// 	"children":[  
-// 	{  
-// 		"id":5,
-// 		"name":"Folder_2.1",
-// 		"children":[  
-
-// 		]
-// 	},
-// 	{  
-// 		"id":6,
-// 		"name":"Folder_2.2",
-// 		"children":[  
-
-// 		]
-// 	}
-// 	],
-// 	"isParent":true
-// },
-// {  
-// 	"id":7,
-// 	"name":"Folder_3",
-// 	"children":[  
-
-// 	]
-// }
-// ];
-
-
-// function renderSelect(obj) {
-// 	for(key in obj) {
-// 		let item = obj[key];
-// 		let name = item.name;
-// 		let id = item.id;
-// 		console.log(item);
-// 		select.append(`
-// 			<option id="${id}">${arrows}${name}</option>
-// 			`);
-
-// 		if (item.children) {
-// 			renderSelect(item.children);
-// 		}
-// 	}
-// }
-
-// renderSelect(folders);
